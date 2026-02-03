7bb16fecd996ac53eb61e17f6a21610b8f76d8ce
= <=>
2026-02-03 12:28:22 +0100

    Correction

--- front-end/js/main.js
+++ front-end/js/main.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
     };
 
     try {
-      const response = await fetch("http://localhost:3000/register", {
+      const response = await fetch("http://localhost:3000/api/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
