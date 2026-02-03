ddec499743c90375bd185fe91851355afa3fa503
= <=>
2026-02-03 12:26:51 +0100

    Suppression fichier en double

--- back-end/src/routes/registerRouteur.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { create } from "../controllers/registerController.js";
-
-const kpoRouter = express.Router();
-
-
-kpoRouter.post("/register", create); 
-
-export default kpoRouter;
